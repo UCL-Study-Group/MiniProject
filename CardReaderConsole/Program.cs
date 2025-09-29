866f386c3ff7ddfb3bacbe226ae1912224f31fba
TheSecondSoda <thomas.lind.spil@gmail.com>
2025-09-29 23:38:05 +0200

    Added NFC Console

---
+++ CardReaderConsole/Program.cs
@@ -0,0 +1,64 @@
+﻿using CardReaderConsole.Models;
+using CardReaderConsole.Services;
+using NFCServiceLibrary.Models;
+using NFCServiceLibrary.Services;
+using NFCServiceLibrary.Services.Interfaces;
+
+namespace CardReaderConsole
+{
+    internal class Program
+    {
+        private static INFCReaderService _nfcService;
+        private static RabbitService _rabbitService;
+
+        private static readonly ConsoleColor _originalColor = Console.BackgroundColor;
+
+        static async Task Main(string[] args)
+        {
+            _nfcService = new NFCReaderService();
+            _rabbitService = new RabbitService();
+
+            _nfcService.CardDetected += OnCardDetected;
+            _nfcService.ErrorOccurred += OnErrorOccurred;
+            _rabbitService.ValidationResponseRecieved += OnValidationRecieved;
+
+            await _rabbitService.StartConsumingValidationResponse();
+
+            if (!_nfcService.StartMonitoring())
+            {
+                Console.WriteLine("Failed to start the NFC monitoring.");
+            }
+
+            Console.ReadLine();
+
+            _nfcService.StopMonitoring();
+            _nfcService.Dispose();
+        }
+
+        private static void OnCardDetected(object sender, CardDetectedEventArgs e)
+        {
+            _rabbitService.PublishMessageAsync("card.validate", e);
+        }
+
+        private static void OnErrorOccurred(object sender, string errorMessage)
+        {
+            Console.WriteLine($"Error: {errorMessage}");
+        }
+
+        private static void OnValidationRecieved(object sender, ValidationModel validation)
+        {
+            ConsoleColor eventColor = validation.ValidationStatus switch
+            {
+                "Valid" => ConsoleColor.Green,
+                "Invalid" => ConsoleColor.Red,
+                _ => _originalColor
+            };
+
+            Console.BackgroundColor = eventColor;
+            Console.Clear();
+            System.Threading.Thread.Sleep(1000);
+            Console.BackgroundColor = _originalColor;
+            Console.Clear();
+        }
+    }
+}
---
+++ CardReaderConsole/Services/RabbitService.cs
@@ -0,0 +1,152 @@
+﻿using CardReaderConsole.Models;
+using FluentResults;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CardReaderConsole.Services
+{
+    public class RabbitService : IAsyncDisposable
+    {
+        private IConnection? _connection;
+        private IChannel? _channel;
+        private AsyncEventingBasicConsumer? _consumer;
+
+        public event EventHandler<ValidationModel> ValidationResponseRecieved;
+
+        private const string ValidationExchange = "validation.exchange";
+        private const string ValidationResponseExchange = "validation.Response.exchange";
+        private const string ValidationResponseQueue = "validation.Response.queue";
+
+        private async Task InitializeAsync()
+        {
+            if (_connection is not null && _connection.IsOpen)
+                return;
+
+            try
+            {
+                var factory = new ConnectionFactory()
+                {
+                    HostName = "localhost",
+                    UserName = "guest",
+                    Password = "guest",
+                    Port = 5672,
+                };
+
+                _connection = await factory.CreateConnectionAsync();
+                _channel = await _connection.CreateChannelAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Countered an exception while trying to establish a connection to RabbitMQ");
+            }
+        }
+
+        private async Task SetupValidationExchange()
+        {
+            await _channel.ExchangeDeclareAsync(ValidationExchange, ExchangeType.Direct, durable: false);
+        }
+
+        private async Task SetupValidationResponseQueue()
+        {
+            await _channel.ExchangeDeclareAsync(ValidationResponseExchange, ExchangeType.Direct, durable: false);
+
+            await _channel.QueueDeclareAsync(ValidationResponseQueue, durable: false);
+
+            await _channel.QueueBindAsync(ValidationResponseQueue, ValidationResponseExchange, "Validation.Response");
+        }
+
+        public async Task<Result> PublishMessageAsync(string route, object body)
+        {
+            try
+            {
+                await InitializeAsync();
+                await SetupValidationExchange();
+
+                if (_connection is null || _channel is null || !_channel.IsOpen)
+                    return Result.Fail("Couldn't initialize RabbitMQ");
+
+                var encodedBody = EncodeMessage(body);
+
+                BasicProperties props = new();
+
+                props.CorrelationId = Guid.NewGuid().ToString();
+                props.ReplyTo = ValidationResponseExchange;
+
+                await _channel.BasicPublishAsync(
+                    exchange: ValidationExchange, 
+                    routingKey: route, 
+                    body: encodedBody, 
+                    basicProperties: props,
+                    mandatory: false);
+
+                return Result.Ok();
+            }
+            catch (Exception)
+            {
+                return Result.Fail("Failed publish message");
+            }
+        }
+
+        public async Task<Result> StartConsumingValidationResponse()
+        {
+            await InitializeAsync();
+            await SetupValidationResponseQueue();
+
+            if (_consumer is not null && !_consumer.IsRunning)
+                return Result.Fail("Consumer is already running");
+
+            if (_consumer is null)
+                _consumer = new AsyncEventingBasicConsumer(_channel);
+
+            _consumer.ReceivedAsync += OnValidationResponse;
+
+            await _channel.BasicConsumeAsync(ValidationResponseQueue, autoAck: true, _consumer);
+            return Result.Ok();
+        }
+
+        private async Task OnValidationResponse(object sender, BasicDeliverEventArgs @event)
+        {
+            try
+            {
+                var message = Encoding.UTF8.GetString(@event.Body.ToArray());
+                var validation = JsonSerializer.Deserialize<ValidationModel>(message);
+
+                if (validation is not null)
+                {
+                    ValidationResponseRecieved.Invoke(this, validation);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error validating inserted card: {ex.Message}");
+            }
+        }
+
+        private static byte[] EncodeMessage(object messageToEncode)
+        {
+            var json = JsonSerializer.Serialize(messageToEncode);
+
+            var encoded = Encoding.UTF8.GetBytes(json);
+
+            return encoded;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_connection is not null && _channel is not null)
+            {
+                await _connection.DisposeAsync();
+                await _channel.DisposeAsync();
+            }
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
