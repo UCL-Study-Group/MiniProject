6795ed4d6d9a943878d8d7e95fec91b062f61739
Esben Dam Cramer <edc@aguardio.com>
2025-09-29 10:13:02 +0200

    Init Checkin

---
+++ CheckinService/Main.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckinServiceo;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class Main : Window
+{
+    public Main()
+    {
+        InitializeComponent();
+    }
+
+    private void UpdateBackground()
+    {
+        ValidationBackground.Background = Brushes.Red;
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        UpdateBackground();
+    }
+}
---
+++ CheckinService/Services/RabbitService.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+
+namespace CheckinServiceo.Services;
+
+public class RabbitService
+{
+    private IConnection? _connection;
+    private IChannel? _channel;
+    
+    public async Task InitializeAsync()
+    {
+        if (_connection is not null && _connection.IsOpen)
+            return;
+
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost"
+        };
+
+        _connection = await factory.CreateConnectionAsync();
+    }
+}
